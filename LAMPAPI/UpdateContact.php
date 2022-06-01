0cada650f83e846cd72d1ddedb6bf4b8534ba85c
DanielG0628 <90162745+DanielG0628@users.noreply.github.com>
2022-06-01 00:36:51 -0400

    Update UpdateContact.php
    doesn't work just yet but looking into it

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,1 +1,48 @@
+<?php 
+    $inData = getRequestInfo();
 
+    $contactName = $inData["contactName"];
+    $contactPhone = $inData["contactPhone"];
+  	$contactEmail = $inData["contactEmail"];
+    $contactId = $inData["contactId"];
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error) 
+	{
+		returnWithError($conn->connect_error);
+	} 
+    else
+    {
+        $stmt = $conn->prepare("UPDATE Contacts SET Name=?,Phone=?,Email=? WHERE ID=?");
+        $stmt->bind_param("sssi", $contactName, $contactPhone, $contactEmail, $contactId);
+        $stmt->execute();
+        
+        if($stmt->error)
+        {
+            returnWithError("Contact cannot be found").
+        }
+        else
+        {
+            returnWithInfo()
+        }
+
+        $stmt->close();
+        $conn->close();
+    }
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+    function returnWithError($err)
+	{
+		sendResultInfoAsJson($err);
+	}
+	
+	function returnWithInfo($updateInfo)
+	{
+		sendResultInfoAsJson($contactName, $contactPhone, $contactEmail, $userId, $contactId);
+	}
+
+?>
